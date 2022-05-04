105f98abf0579d973ff5d9c971db3d86471ad388
liangjin.ct@alibaba-inc.com <liangjin.ct@alibaba-inc.com>
2022-05-04 21:17:12 +0800

    vga, 0xc0000000 testing

--- init/main.c
+++ init/main.c
@@ -1,5 +1,15 @@
+#include <fairy.h>
+#include <fairy/stdint.h>
 
 int start_kernel()
 {
+    uint8_t     *vmem = (uint8_t*)0xC00B8000;
+    int         i;
+    uint8_t     mem[24] = "HHEELLLLOO\n\n";
+
+    for (i = 0; i < 12; i++) {
+        vmem[i] = mem[i];
+    }
+
     while (1);
 }
